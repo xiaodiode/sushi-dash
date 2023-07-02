4d7715252a8df27fa32b3294a2913d08d8a67163
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-07-01 21:45:46 -0400

    added level button unlocked visuals
    modified the interactable and transparency properties of the button depending on whether the level is unlocked

--- Sushi Dash/Assets/Scripts/LevelButton.cs
+++ Sushi Dash/Assets/Scripts/LevelButton.cs
@@ -10,14 +10,17 @@ public class LevelButton : MonoBehaviour
     private GameManager gameManager;
     private Button button;
     private TextMeshProUGUI levelText;
+    private Image locked;
     private int level;
     private bool first_clear, unlocked;
+    private float lockedAlpha = 0.5f;
+
+    Color tempColor;
     // Start is called before the first frame update
     void Start()
     {
-        button = gameObject.GetComponent<Button>();
         button.onClick.AddListener(levelSelected);
-        
+
     }
 
     // Update is called once per frame
@@ -40,13 +43,25 @@ public class LevelButton : MonoBehaviour
         gameManager = newGameManager;
     }
     public void setLevel(int newLevel){
+        button = transform.Find("Level Button").GetComponent<Button>();
         level = newLevel;
-        levelText = transform.Find("LvlText").GetComponent<TextMeshProUGUI>();
+        levelText = button.transform.Find("LvlText").GetComponent<TextMeshProUGUI>();
         levelText.text = "Level " + level.ToString();
         
     }
     public void setUnlocked(bool boolean){
+        tempColor = button.GetComponent<Image>().color;
+        tempColor.a = lockedAlpha;
+        
+        Debug.Log("locked object: " + locked);
         unlocked = boolean;
+        if(boolean){
+            button.interactable = true;
+        }
+        else{
+            button.interactable = false;
+            button.GetComponent<Image>().color = tempColor;
+        }
     }
 
     public void setFirstClear(bool boolean){
