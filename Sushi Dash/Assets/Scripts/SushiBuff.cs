b059cd1b9304508cb83eecbd855dfa0a9d8ccc10
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2023-07-07 17:17:23 -0400

    sushi buff text updates properly

--- Sushi Dash/Assets/Scripts/SushiBuff.cs
+++ Sushi Dash/Assets/Scripts/SushiBuff.cs
@@ -6,7 +6,6 @@ using TMPro;
 
 public class SushiBuff : MonoBehaviour
 {
-    private Button button;
     public string sushiName;
     public int level;
     public string rarity, buffDescription;
@@ -14,10 +13,29 @@ public class SushiBuff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        button = gameObject.GetComponent<Button>();
         level = 0;
-        buffDescription = "Skill: ";
 
+        // switch(sushiName){
+        //     case "Salmon Nigiri":
+        //         salmonBuff();
+        //         break;
+        //     case "Tuna Nigiri":
+        //         tunaBuff();
+        //         break;
+        //     case "Egg Nigiri":
+        //         eggBuff();
+        //         break;
+        // }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setSushiName(string newSushiName){
+        sushiName = newSushiName;
         switch(sushiName){
             case "Salmon Nigiri":
                 salmonBuff();
@@ -29,30 +47,29 @@ public class SushiBuff : MonoBehaviour
                 eggBuff();
                 break;
         }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void setSushiName(string newSushiName){
-        sushiName = newSushiName;
     }
 
     private void salmonBuff(){
         baseBuff = .01f;
         buffRate = .01f;
         rarity = "Common";
-        buffDescription = "Speed up the sushi-making progress by <color=red>" + (100*(baseBuff + (buffRate*level))).ToString() + 
+        buffDescription = "Speed up the sushi-making progress by \n<color=red>" + (100*(baseBuff + (buffRate*level))).ToString() + 
         "%</color> (+" + (100*buffRate).ToString() + "% / level)";
     }
     private void tunaBuff(){
+        baseBuff = 5;
+        buffRate = 5;
         rarity = "Common";
+        buffDescription = "Start each game with <color=red>\n" + (baseBuff + (buffRate*level)).ToString() + 
+        "</color> coins (+" + (buffRate).ToString() + " coins / level)";
+
     }
     private void eggBuff(){
+        baseBuff = .01f;
+        buffRate = .01f;
         rarity = "Common";
+        buffDescription = "Slow down customers' walking speed by \n<color=red>" + (100*(baseBuff + (buffRate*level))).ToString() + 
+        "%</color> (+" + (100*buffRate).ToString() + "% / level)";
     }
 
     public void setSushiDescription(){
@@ -60,5 +77,16 @@ public class SushiBuff : MonoBehaviour
     }
     public void increaseLevel(){
         level+=1;
+        switch(sushiName){
+            case "Salmon Nigiri":
+                salmonBuff();
+                break;
+            case "Tuna Nigiri":
+                tunaBuff();
+                break;
+            case "Egg Nigiri":
+                eggBuff();
+                break;
+        }
     }
 }
